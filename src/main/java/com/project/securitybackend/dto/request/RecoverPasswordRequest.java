8568ec98d47211b8d423b43e047523d04ba3743f
somara <ms.mishoni@gmail.com>
2021-06-09 00:18:47 +0200

    Logging implemented

---
+++ src/main/java/com/project/securitybackend/dto/request/RecoverPasswordRequest.java
@@ -0,0 +1,18 @@
+package com.project.securitybackend.dto.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+public class RecoverPasswordRequest {
+
+    @Size(min = 8, max = 50, message = "The password must contain at least 8 characters")
+    @Pattern(regexp = "^((?=.*\\d)(?=.*[A-Z])(?=.*\\W))$", message = "Your password must contain at least one uppercase letter, one number and one special character")
+    private String password;
+
+    @Size(min = 8, max = 50, message = "The password must contain at least 8 characters")
+    @Pattern(regexp = "^((?=.*\\d)(?=.*[A-Z])(?=.*\\W))$", message = "Your password must contain at least one uppercase letter, one number and one special character")
+    private String repeatedPassword;
+}
