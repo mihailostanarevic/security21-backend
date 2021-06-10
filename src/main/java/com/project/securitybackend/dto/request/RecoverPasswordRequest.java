3d2f3a0b07d4e0ed4b1b2b8b715ec4f0dc98eed0
Aleksandar Beker <a.beker@vegait.rs>
2021-06-10 18:24:42 +0200

    merge conflicts resolved

--- src/main/java/com/project/securitybackend/dto/request/RecoverPasswordRequest.java
+++ src/main/java/com/project/securitybackend/dto/request/RecoverPasswordRequest.java
@@ -9,11 +9,11 @@ import javax.validation.constraints.Size;
 public class RecoverPasswordRequest {
 
     @Size(min = 8, max = 50, message = "The password must contain at least 8 characters")
-    @Pattern(regexp = "^((?=.*\\d)(?=.*[A-Z])(?=.*\\W))$", message = "Your password must contain at least one uppercase letter, one number and one special character")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&].{9,}", message = "Your password must contain at least one uppercase letter, one number and one special character")
     private String password;
 
     @Size(min = 8, max = 50, message = "The password must contain at least 8 characters")
-    @Pattern(regexp = "^((?=.*\\d)(?=.*[A-Z])(?=.*\\W))$", message = "Your password must contain at least one uppercase letter, one number and one special character")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&].{9,}", message = "Your password must contain at least one uppercase letter, one number and one special character")
     private String repeatedPassword;
 
     private String user;
